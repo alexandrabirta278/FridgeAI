15fdad07606ca5fd7afc061f53db78c9038d1553
Alexandra Birta <alexandra.birta2002@gmail.com>
2025-05-21 20:10:59 +0300

    fix

--- my-app/pages/index.js
+++ my-app/pages/index.js
@@ -13,7 +13,7 @@ const geistMono = Geist_Mono({
 
 export default function Home() {
   return (
-    <div className="p-8 ">
+    <div>
       <h1 className="text-4xl font-bold mb-4">Welcome to My App</h1>
     </div>
   );
