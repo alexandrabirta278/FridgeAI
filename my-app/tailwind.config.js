fc9d20784f6b5ad59dc688a02b348c50dadfe213
Alexandra Birta <alexandra.birta2002@gmail.com>
2025-05-24 13:14:30 +0300

    made UI better

--- my-app/tailwind.config.js
+++ my-app/tailwind.config.js
@@ -1,15 +1,18 @@
+/** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
   theme: {
     extend: {
       fontFamily: {
-        inter: ['Inter', 'sans-serif'],
-      },
-      colors: {
-        primary: '#f43f5e',
-        secondary: '#fcd34d',
+        sans: ['Inter', 'sans-serif'],
       },
     },
   },
-  plugins: [require('@tailwindcss/forms'), require('@tailwindcss/typography')],
+  plugins: [
+    require('@tailwindcss/forms'),
+    require('@tailwindcss/typography'),
+  ],
 }
